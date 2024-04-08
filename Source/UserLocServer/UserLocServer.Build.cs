71ec0ee3fe921c8c0e5937cc77bb4116803cc7e7
haugeSander <98831002+haugeSander@users.noreply.github.com>
2024-04-08 17:21:08 -0400

    Change: new settings, icon and thumbnail. Issue: unable to cook, uncertain of error.

--- Source/UserLocServer/UserLocServer.Build.cs
+++ Source/UserLocServer/UserLocServer.Build.cs
@@ -10,7 +10,7 @@ public class UserLocServer : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Sockets", "Networking" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "HTTP" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
