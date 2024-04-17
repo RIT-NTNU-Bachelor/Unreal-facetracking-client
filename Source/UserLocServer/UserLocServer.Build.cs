4e8586ff587580170031576de0419565d86cdc19
haugeSander <98831002+haugeSander@users.noreply.github.com>
2024-04-17 16:45:11 -0400

    Fix: lighting and level direction for packaging

--- Source/UserLocServer/UserLocServer.Build.cs
+++ Source/UserLocServer/UserLocServer.Build.cs
@@ -8,7 +8,7 @@ public class UserLocServer : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Sockets", "Networking" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "RenderCore", "InputCore", "Sockets", "Networking" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "HTTP" });
 	}
